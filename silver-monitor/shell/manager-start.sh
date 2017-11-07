1cf5676cb15dbeb73b9075180ad3255fd6986bd9
wuleying <5590548@qq.com>
2017-11-07 15:08:54 +0800

    update

--- silver-monitor/shell/manager-start.sh
+++ silver-monitor/shell/manager-start.sh
@@ -2,3 +2,17 @@
 
 # shellcheck disable=SC1091
 source ./shell/common.sh
+
+start() {
+    # 杀死原进程
+    "$ENV_SHELL_DIR"/kill.sh manager
+
+    # 执行
+    nohup "$ENV_BIN_DIR"/"$PROJECT_NAME"-"$MANAGER_NAME" --config "$ENV_CONFIG_DIR"/config-dev.ini &
+}
+
+main(){
+    start "$@"
+}
+
+main "$@"
--- silver-monitor/shell/manager-stop.sh
+++ silver-monitor/shell/manager-stop.sh
@@ -2,3 +2,14 @@
 
 # shellcheck disable=SC1091
 source ./shell/common.sh
+
+stop() {
+    # 杀死原进程
+    "$ENV_SHELL_DIR"/kill.sh manager
+}
+
+main(){
+    stop "$@"
+}
+
+main "$@"
