eabb666d676b5c09b81f615a586635303119a7f9
wuleying <5590548@qq.com>
2017-11-08 14:37:24 +0800

    update

--- silver-monitor/shell/kill-all.sh
+++ silver-monitor/shell/kill-all.sh
@@ -6,7 +6,7 @@
 source ./shell/common.sh
 
 kill_all(){
-    pgrep -f "silver-monitor-server" | xargs kill -9
+    pgrep -f "silver-monitor-server"  | xargs kill -9
     pgrep -f "silver-monitor-manager" | xargs kill -9
 
     _info "Is all stopped."
