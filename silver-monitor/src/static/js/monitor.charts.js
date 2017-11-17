06a4bccf1a326ac5885ff0059998312070ba805a
wuleying <5590548@qq.com>
2017-11-17 17:05:59 +0800

    update

--- silver-monitor/src/static/js/monitor.charts.js
+++ silver-monitor/src/static/js/monitor.charts.js
@@ -1,16 +1,4 @@
-function showChart() {
-    var base = new Date(1968, 9, 3);
-    var oneDay = 86400;
-    var date = [];
-
-    var data = [Math.random() * 300];
-
-    for (var i = 1; i < 20000; i++) {
-        var now = new Date(base += oneDay);
-        date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
-        data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
-    }
-
+function showChart(date, data) {
     option = {
         tooltip: {
             trigger: 'axis',
