a525b4e7e3f736a3a1a0e8f8248fd44a911c3537
wuleying <5590548@qq.com>
2017-12-29 17:57:21 +0800

    update silver-blockchain

--- silver-blockchain/src/block/blockchain.go
+++ silver-blockchain/src/block/blockchain.go
@@ -1,15 +1,15 @@
-package main
+package block
 
 // 区块链结构体
 type Blockchain struct {
-	blocks []*Block
+	Blocks []*Block
 }
 
 // 将区块加入区块链
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	bc.Blocks = append(bc.Blocks, newBlock)
 }
 
 // 创建新区块链
--- silver-blockchain/src/main.go
+++ silver-blockchain/src/main.go
@@ -1,14 +1,15 @@
 package main
 
 import "fmt"
+import b "go-labs/silver-blockchain/src/block"
 
 func main() {
-	bc := NewBlockchain()
+	bc := b.NewBlockchain()
 
 	bc.AddBlock("First block.")
 	bc.AddBlock("Second block.")
 
-	for _, block := range bc.blocks {
+	for _, block := range bc.Blocks {
 		fmt.Printf("PrevBlockHash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
