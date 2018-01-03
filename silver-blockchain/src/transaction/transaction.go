6bb084266a08930ce6ab0b9647125eee29b8c873
wuleying <5590548@qq.com>
2018-01-03 16:55:00 +0800

    Add Transaction class.

---
+++ silver-blockchain/src/transaction/transaction.go
@@ -0,0 +1,83 @@
+package transaction
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"go-labs/silver-blockchain/src/block"
+	"log"
+)
+
+const subsidy = 10
+
+type Transaction struct {
+	Id  []byte
+	In  []TInput
+	Out []TOutput
+}
+
+type TInput struct {
+	Id        []byte
+	Out       int
+	ScriptSig string
+}
+
+type TOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+func (t Transaction) IsCoinBase() bool {
+	return len(t.In) == 1 && len(t.In[0].Id) == 0 && t.In[0].Out == -1
+}
+
+func (t Transaction) SetId() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	encoder := gob.NewEncoder(&encoded)
+
+	err := encoder.Encode(t)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	hash = sha256.Sum256(encoded.Bytes())
+	t.Id = hash[:]
+}
+
+func (in TInput) CanUnlockOutputWith(data string) bool {
+	return in.ScriptSig == data
+}
+
+func (out TOutput) CanBeUnlockWith(data string) bool {
+	return out.ScriptPubKey == data
+}
+
+func NewCoinbase(to string, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+
+	tIn := TInput{[]byte{}, -1, data}
+	tOut := TOutput{subsidy, to}
+
+	t := Transaction{nil, []TInput{tIn}, []TOutput{tOut}}
+	t.SetId()
+
+	return t
+}
+
+func NewUTXOTransaction(from string, to string, amout int, bc *block.Blockchain) *Transaction {
+	var inputs []TInput
+	var outputs []TOutput
+
+	// todo
+
+	t := Transaction{nil, inputs, outputs}
+	t.SetId()
+
+	return t
+}
