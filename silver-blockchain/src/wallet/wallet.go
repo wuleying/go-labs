754a17fb7ad3bc14da4bdc0f57f4d82d174a8e85
wuleying <5590548@qq.com>
2018-01-11 15:30:51 +0800

    update

---
+++ silver-blockchain/src/wallet/wallet.go
@@ -0,0 +1,84 @@
+package wallet
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"go-labs/silver-blockchain/src/utils"
+	"golang.org/x/crypto/ripemd160"
+	"log"
+)
+
+const version = byte(0x00)
+const addressChecksumLen = 4
+
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+func (w Wallet) GetAddress() []byte {
+	publicKeyHash := HashPublicKey(w.PublicKey)
+
+	versionedPayload := append([]byte(version), publicKeyHash...)
+	checksum := checksum(versionedPayload)
+
+	fullPayload := append(versionedPayload, checksum...)
+	address := utils.Base58Encode(fullPayload)
+
+	return address
+}
+
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
+
+func HashPublicKey(publicKey []byte) []byte {
+	publicSha256 := sha256.Sum256(publicKey)
+
+	ripemd160Hasher := ripemd160.New()
+
+	_, err := ripemd160Hasher.Write(publicSha256[:])
+	if err != nil {
+		log.Panic(err)
+	}
+
+	publicRipemd160 := ripemd160Hasher.Sum(nil)
+
+	return publicRipemd160
+}
+
+func ValidateAddress(address string) bool {
+	publicKeyHash := utils.Base58Decode([]byte(address))
+	actualChecksum := publicKeyHash[len(publicKeyHash)-addressChecksumLen]
+	version := publicKeyHash[0]
+	publicKeyHash = publicKeyHash[1 : len(publicKeyHash)-addressChecksumLen]
+	targetChecksum := checksum(append([]byte(version), publicKeyHash...))
+
+	return bytes.Compare(actualChecksum, targetChecksum) == 0
+}
+
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	public := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes())
+
+	return *private, public
+}
+
+func checksum(payload []byte) []byte {
+	firstSha := sha256.Sum256(payload)
+	secondSha := sha256.Sum256(firstSha[:])
+
+	return secondSha[:addressChecksumLen]
+}
---
+++ silver-blockchain/src/wallet/wallets.go
@@ -0,0 +1,50 @@
+package wallet
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const walletFile = "wallet_%s.dat"
+
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+func NewWallets() (Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallet)
+
+	err := wallets.LoadFromFile()
+
+	return &wallets, err
+}
+
+func (ws *Wallets) LoadFromFile() error {
+	if _, err := os.Stat(walletFile); os.IsNotExist(err) {
+		return err
+	}
+
+	fileContent, err := ioutil.ReadFile(walletFile)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var wallets Wallets
+
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+
+	err = decoder.Decode(&wallets)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ws.Wallets = wallets.Wallets
+
+	return nil
+}
