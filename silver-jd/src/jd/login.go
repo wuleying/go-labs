8e0ee3ffacb7021fa6f81ee9db5a4a0b8f588843
wuleying <5590548@qq.com>
2018-01-20 12:27:43 +0800

    Update

--- silver-jd/src/jd/login.go
+++ silver-jd/src/jd/login.go
@@ -1,8 +1,10 @@
 package jd
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	sjson "github.com/bitly/go-simplejson"
 	"github.com/go-clog/clog"
 	"io"
 	"io/ioutil"
@@ -226,4 +228,112 @@ func (jd *JingDong) validateQRToken(URL string) error {
 
 	resp.Body.Close()
 	return nil
+}
+
+func (jd *JingDong) waitForScan(URL string) error {
+	var (
+		err    error
+		req    *http.Request
+		resp   *http.Response
+		wlfstk string
+	)
+
+	for _, c := range jd.jar.Cookies(nil) {
+		if c.Name == "wlfstk_smdl" {
+			wlfstk = c.Value
+			break
+		}
+	}
+
+	u, _ := url.Parse(URL)
+	q := u.Query()
+	q.Set("callback", "jQuery123456")
+	q.Set("appid", strconv.Itoa(133))
+	q.Set("token", wlfstk)
+	q.Set("_", strconv.FormatInt(time.Now().Unix()*1000, 10))
+	u.RawQuery = q.Encode()
+
+	if req, err = http.NewRequest("GET", u.String(), nil); err != nil {
+		clog.Info("请求（%+v）失败: %+v", URL, err)
+		return err
+	}
+
+	// mush have
+	req.Host = "qr.m.jd.com"
+	req.Header.Set("Referer", "https://passport.jd.com/new/login.aspx")
+	applyCustomHeader(req, DefaultHeaders)
+
+	for retry := 50; retry != 0; retry-- {
+		if resp, err = jd.client.Do(req); err != nil {
+			clog.Info("二维码失效：%+v", err)
+			break
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			respMsg := string(responseData(resp))
+			resp.Body.Close()
+
+			n1 := strings.Index(respMsg, "(")
+			n2 := strings.Index(respMsg, ")")
+
+			var js *sjson.Json
+			if js, err = sjson.NewJson([]byte(respMsg[n1+1 : n2])); err != nil {
+				clog.Error(0, "解析响应数据失败: %+v", err)
+				clog.Trace("Response data  : %+v", respMsg)
+				clog.Trace("Response Header: %+v", resp.Header)
+				break
+			}
+
+			code := js.Get("code").MustInt()
+			if code == 200 {
+				jd.token = js.Get("ticket").MustString()
+				clog.Info("token : %+v", jd.token)
+				break
+			} else {
+				clog.Info("%+v : %s", code, js.Get("msg").MustString())
+				time.Sleep(time.Second * 3)
+			}
+		} else {
+			resp.Body.Close()
+		}
+	}
+
+	if jd.token == "" {
+		err = fmt.Errorf("未检测到QR扫码结果")
+		return err
+	}
+
+	return nil
+}
+
+func applyCustomHeader(req *http.Request, header map[string]string) {
+	if req == nil || len(header) == 0 {
+		return
+	}
+
+	for key, val := range header {
+		req.Header.Set(key, val)
+	}
+}
+
+func responseData(resp *http.Response) []byte {
+	if resp == nil {
+		return nil
+	}
+
+	var reader io.Reader
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, _ = gzip.NewReader(resp.Body)
+	default:
+		reader = resp.Body
+	}
+
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		clog.Error(0, "读取响应数据失败: %+v", err)
+		return nil
+	}
+
+	return data
 }
