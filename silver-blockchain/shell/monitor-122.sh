db99bd7da1436f2170c7199ef49bbef70d6c8b1b
wuleying <5590548@qq.com>
2018-01-24 12:37:54 +0800

    Update

--- silver-blockchain/shell/monitor-122.sh
+++ silver-blockchain/shell/monitor-122.sh
@@ -6,10 +6,8 @@ PARAMS="page=0&glbm=110000000400&hpzl=52&type=0"
 while true
 do
     result=$(curl -s -XPOST -d "${PARAMS}" "${URL}" | jq ".data.list.content[0].subhd")
-    echo ${result}
-    echo ${result:3:1}
-    echo ${result:4:1}
-    echo ${result:5:1}
+    echo ${result} - ${result:3:1} ${result:4:1} ${result:5:1}
+    date
 
     if [ "${result:3:1}" == "${result:4:1}" -a "${result:4:1}" == "${result:5:1}" ]; then
         osascript -e "display notification \"豹子号段出现: ${result}\" with title \"122 Monitor\""
