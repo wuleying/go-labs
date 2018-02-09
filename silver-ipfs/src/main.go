01e66db935fe715e2eb7485f87ba5ff39b6c638d
wuleying <5590548@qq.com>
2018-02-09 10:54:20 +0800

    Init project silver-ipfs.

---
+++ silver-ipfs/src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-clog/clog"
+	"os"
+)
+
+func init() {
+	if err := clog.New(clog.CONSOLE, clog.ConsoleConfig{
+		Level:      clog.INFO,
+		BufferSize: 100},
+	); err != nil {
+		fmt.Printf("init console log failed. error %+v.", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	defer clog.Shutdown()
+
+	clog.Info("hello world.")
+}
