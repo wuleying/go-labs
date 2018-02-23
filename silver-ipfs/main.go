90adbd0c51843857d6d120c2ba8f29b95b01bc58
wuleying <5590548@qq.com>
2018-02-23 17:09:32 +0800

    Update

--- silver-ipfs/main.go
+++ silver-ipfs/main.go
@@ -25,7 +25,7 @@ func main() {
 	err, result := util.ExecCommand(command, params)
 
 	if err != nil {
-		clog.Fatal(2, err.Error())
+		clog.Fatal(util.CLOG_SKIP_DISPLAY_INFO, err.Error())
 	}
 
 	clog.Info(result)
