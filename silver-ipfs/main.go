9d97151fb249228cc6c600fbf047ec7ae42fc50a
wuleying <5590548@qq.com>
2018-02-24 14:46:12 +0800

    Update

--- silver-ipfs/main.go
+++ silver-ipfs/main.go
@@ -21,9 +21,12 @@ func init() {
 func main() {
 	defer clog.Shutdown()
 
-	//err, out := commands.AddFile("/Users/luoliang/Desktop/test.txt")
-	err, out := commands.CatFile("QmXsjqFzpz5e7qC2fkPb12HiMPtj81BXrJBfC5zWkJRPcP")
+	err, out := commands.AddFile("/Users/luoliang/Desktop/test.txt")
+	if err != nil {
+		clog.Fatal(util.CLOG_SKIP_DISPLAY_INFO, err.Error())
+	}
 
+	err, out = commands.CatFile("QmXsjqFzpz5e7qC2fkPb12HiMPtj81BXrJBfC5zWkJRPcP")
 	if err != nil {
 		clog.Fatal(util.CLOG_SKIP_DISPLAY_INFO, err.Error())
 	}
