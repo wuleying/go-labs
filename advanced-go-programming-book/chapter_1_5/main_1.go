262c0878b81636cbaee37bb86c9a7a042c476887
wuleying <5590548@qq.com>
2018-07-26 21:19:11 +0800

    Update.

---
+++ advanced-go-programming-book/chapter_1_5/main_1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	done := make(chan int, 10) // 带 10 个缓存
+
+	// 开N个后台打印线程
+	for i := 0; i < cap(done); i++ {
+		go func() {
+			fmt.Printf("Hello, world. %d\n", i)
+			done <- 1
+		}()
+	}
+
+	// 等待N个后台线程完成
+	for i := 0; i < cap(done); i++ {
+		<-done
+	}
+}
