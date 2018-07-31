326b36c69c2745d29bac6b7824780e9bb52c245d
wuleying <5590548@qq.com>
2018-07-31 11:18:27 +0800

    Update.

---
+++ advanced-go-programming-book/chapter_3_8/main_0.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"runtime"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	print(GetGoid())
+}
+
+func GetGoid() int64 {
+	var (
+		buf [64]byte
+		n   = runtime.Stack(buf[:], false)
+		stk = strings.TrimPrefix(string(buf[:n]), "goroutine ")
+	)
+
+	idField := strings.Fields(stk)
+	id, err := strconv.Atoi(idField[0])
+	if err != nil {
+		panic(fmt.Errorf("can not get goroutine id: %v", err))
+	}
+
+	return int64(id)
+}
