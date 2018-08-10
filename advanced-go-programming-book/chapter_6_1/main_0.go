c32aeff70f086f2a15e6135774d87f1761ee00f3
wuleying <5590548@qq.com>
2018-08-10 16:45:42 +0800

    Add new project is 'silver-ui'

---
+++ advanced-go-programming-book/chapter_6_1/main_0.go
@@ -0,0 +1,23 @@
+package main
+
+import "sync"
+
+var counter int
+
+func main() {
+	var wg sync.WaitGroup
+	var l sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			l.Lock()
+			counter++
+			l.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	println(counter)
+}
---
+++ silver-ui/version/version.go
@@ -0,0 +1,4 @@
+package version
+
+// Version 系统版本
+var Version = "unknown"
