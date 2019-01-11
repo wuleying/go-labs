1020aef4e87de2053df1d2119819702f3ebd6de7
wuleying <5590548@qq.com>
2019-01-11 16:39:08 +0800

    Modify demo.

--- silver-errgroup/main.go
+++ silver-errgroup/main.go
@@ -15,24 +15,24 @@ func main() {
 	for index := 0; index < 3; index++ {
 		indexTmp := index
 		group.Go(func() error {
-			fmt.Sprintln("", indexTmp)
+			fmt.Printf("indexTmp = %d\n", indexTmp)
 			if indexTmp == 0 {
-				fmt.Println("indexTemp == 0 end ")
+				fmt.Println("indexTmp == 0 end ")
 			} else if indexTmp == 1 {
-				fmt.Println("indexTemp == 1 start ")
+				fmt.Println("indexTmp == 1 start ")
 				// 这里一般都是某个协程发生异常之后，调用cancel()
 				// 样别的协程就可以通过errCtx获取到err信息，以便决定是否需要取消后续操作
 				cancel()
-				fmt.Println("indexTemp == 1 had err ")
+				fmt.Println("indexTmp == 1 had err ")
 			} else if indexTmp == 2 {
-				fmt.Println("indexTemp == 2 begin ")
+				fmt.Println("indexTmp == 2 begin ")
 				time.Sleep(1 * time.Second)
 				// 检查 其他协程已经发生错误，如果已经发生异常，则不再执行下面的代码
 				err := CheckGoroutineErr(errCtx)
 				if err != nil {
 					return err
 				}
-				fmt.Println("indexTemp == 2 end ")
+				fmt.Println("indexTmp == 2 end ")
 			}
 			return nil
 		})
